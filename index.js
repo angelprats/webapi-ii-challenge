f0781bd9f1258303e10be21afc78fb69e07f7931
angel prats <angelpratz@gmail.com>
2019-07-09 15:59:16 -0400

    created request /api/posts/:id/comment

--- index.js
+++ index.js
@@ -22,6 +22,28 @@ server.post('/api/posts', (req, res) => {
     }
 })
 
+server.post('/api/posts/:id/comments', (req, res) => {
+   const id = req.params.comments;
+   const comment = req.body;
+   db.insertComment()
+   .then(post => {
+       if(post) {
+           if(!comment.title || !comment.contents) {
+               db.insertComment(id, comment)
+               .then(updated => res.status(200).json(updated))
+               .catch(err => res.status(500).json({ error: 'There was an error while saving the comment to the database"'}))
+           }
+           else {
+               res.status(400).json({ errorMessage: 'Please provide text for the comment.'})
+           }
+       }
+       else {
+           res.status(404).json({ message: 'The post with the specified ID does not exist.' })
+       }
+    })
+    .catch(err => res.status(500).json)({ error: 'There was an error while saving the comment to the database' })
+})
+
 
 server.listen(7000, () => console.log('express server running on port 7000'));
   
