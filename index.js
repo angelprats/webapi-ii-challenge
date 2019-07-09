b7be1817517fb633bad35146bbc9e1d5e0c959e3
angel prats <angelpratz@gmail.com>
2019-07-09 15:43:19 -0400

    created sever post

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ server.get('/', (req, res) => {
     res.send('Hello World!!!')
 })
 
+server.post('/api/posts', (req, res) => {
+    const apiInfo = req.body;
+    if(!apiInfo.title || !apiInfo.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    else {
+        db.insert(apiInfo)
+        .then(post => res.status(201).json(post))
+        .catch(err => res.status(500).json({ error: "There was an error while saving the post to the database"}))
+    }
+})
+
+
 server.listen(7000, () => console.log('express server running on port 7000'));
+  
